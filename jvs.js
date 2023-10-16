cd868b7ebdd40c9e273e3e4e797c08a7052446f5
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-16 14:40:19 +0300

    Added round() function to play one round of Rock,paper,Scissors

--- jvs.js
+++ jvs.js
@@ -5,3 +5,23 @@ function getComputerChoice() {
 
     return choices[index];
 }
+
+function round(pc, cc) {
+    let result;
+    pc = pc.toLowerCase();
+    if (pc === cc) result = "It's a tie";
+    else {
+        if (pc === "rock") {
+            if (cc === "scissors") result = "You Win, Rock beats Scissors";
+            if (cc === "paper") result = "You Lose, Paper beats Rock";
+        } else if (pc === "scissors") {
+            if (cc === "paper") result = "You Win, Scissors beats Paper";
+            if (cc === "rock") result = "You Lose, Rock beats Scissors";
+        } else if (pc === "paper") {
+            if (cc === "scissors") result = "You Lose, Scissors beats Paper";
+            if (cc === "rock") result = "You Win, Paper beats Rock";
+        } else result = "error";
+    }
+    return result;
+}
+console.log(round(prompt("Rock, Paper, Scissors, Your choice"), getComputerChoice()));
